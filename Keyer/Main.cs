057e23915c3155480fb09cef88f7203788332433
kuruchin <kuruchin>
2024-02-09 18:07:19 +0300

    First commit. Created project.

---
+++ Keyer/Main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Keyer
+{
+    public partial class Keyer : Form
+    {
+        public Keyer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
