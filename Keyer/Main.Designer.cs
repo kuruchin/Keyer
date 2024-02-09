6b8f1d5d59e73244e3dfb8c55492edd61490d453
kuruchin <kuruchin>
2024-02-09 19:06:06 +0300

    Builded simple functionality

--- Keyer/Main.Designer.cs
+++ Keyer/Main.Designer.cs
@@ -30,28 +30,66 @@ namespace Keyer
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(652, 379);
+            this.button1.Location = new System.Drawing.Point(53, 75);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(136, 59);
             this.button1.TabIndex = 0;
             this.button1.Text = "Выбрать картинку";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.BrowseButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(53, 296);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 59);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Сохранить картинку";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(250, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(320, 180);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(250, 229);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(320, 180);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
             // 
             // Keyer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(604, 450);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Keyer";
-            this.Text = "Form1";
+            this.Text = "Keyer";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -59,6 +97,9 @@ namespace Keyer
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- Keyer/Main.cs
+++ Keyer/Main.cs
@@ -1,17 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Imaging;
 using System.Windows.Forms;
 
 namespace Keyer
 {
     public partial class Keyer : Form
     {
+        private Color keyColor = Color.FromArgb(0, 205, 24);
+        private Bitmap originalImage;
+        private Bitmap processedImage;
+
+
         public Keyer()
         {
             InitializeComponent();
@@ -22,9 +22,66 @@ namespace Keyer
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void BrowseButton_Click(object sender, EventArgs e)
         {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "PNG Files|*.png",
+                Title = "Select an Image File"
+            };
 
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                LoadImage(selectedFilePath);
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (processedImage != null)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "PNG Files|*.png",
+                    Title = "Save Processed Image"
+                };
+
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string savePath = saveFileDialog.FileName;
+                    processedImage.Save(savePath, ImageFormat.Png);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please load and process an image first.", "No Processed Image", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void LoadImage(string filePath)
+        {
+            originalImage = new Bitmap(filePath);
+
+            pictureBox1.Image = originalImage;
+
+            processedImage = new Bitmap(originalImage.Width, originalImage.Height, PixelFormat.Format32bppArgb);
+
+            ProcessImage();
+        }
+
+        private void ProcessImage()
+        {
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color pixelColor = originalImage.GetPixel(x, y);
+                    processedImage.SetPixel(x, y, (pixelColor == keyColor) ? Color.Transparent : pixelColor);
+                }
+            }
+
+            pictureBox2.Image = processedImage;
         }
     }
 }
